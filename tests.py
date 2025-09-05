4ce8a0fa4a9a6e241507399c783a990bb8125505
Mateus Silva <dev.mateushssilva@gmail.com>
2025-09-04 21:22:35 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -34,3 +34,100 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+def test_remove_invalid_choice_id():
+    question = Question(title='q1')
+
+    question.add_choice('a', True)
+
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(3)
+
+def test_remove_all_choices():
+    question = Question(title='q1')
+
+    question.add_choice('a', True)
+    question.add_choice('b', False)
+
+    assert len(question.choices) == 2
+
+    question.remove_all_choices()
+
+    assert len(question.choices) == 0
+
+def test_set_correct_choice():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+
+    question.set_correct_choices([1])
+
+    choice = question.choices[0]
+    choice2 = question.choices[1]
+
+    assert choice.is_correct
+    assert not choice2.is_correct 
+
+def test_set_more_than_max_selections_correct_choices():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+    question.add_choice('b', False)
+
+    with pytest.raises(Exception):
+        question.set_correct_choices([1, 2, 3])
+
+def test_set_invalid_correct_choice_id():
+    question = Question(title='q1')
+
+    question.add_choice('a', False)
+
+    with pytest.raises(Exception):
+        question.set_correct_choices([3])
+
+def test_get_correct_selected_choices():
+    question = Question(title='q1', points=1, max_selections=2)
+
+    question.add_choice('a', False)
+    question.add_choice('b', True)
+    question.add_choice('c', True)
+
+    selected_choices = [1, 2]
+
+    assert question.correct_selected_choices(selected_choices) == [2]
+
+def test_create_question_with_invalid_points():
+    with pytest.raises(Exception):
+        Question('q1', points=-3)
+    with pytest.raises(Exception):
+        Question('q1', points=500)
+
+def test_create_choice_with_invalid_text():
+    question = Question('q1')
+
+    with pytest.raises(Exception):
+        question.add_choice('', False)
+
+    with pytest.raises(Exception):
+        question.add_choice('a'*300, True)
+
+def test_remove_all_choices_from_question_with_no_choices():
+    question = Question('q1')
+
+    assert len(question.choices) == 0
+
+    question.remove_all_choices()
+
+    assert len(question.choices) == 0
+
+def test_multiple_correct_choices_with_max_selections_1():
+    question = Question('q1', points=1, max_selections=1)
+
+    question.add_choice('a', True)
+    question.add_choice('b', True)
+    question.add_choice('c', False)
+
+    assert question.correct_selected_choices([1]) == [1]
+
+    assert question.correct_selected_choices([2]) == [2]
